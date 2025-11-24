34c2dc536851445414598744e2ca38eaea3671aa
Shubham Kumar Sahu <shubham.official571@gmail.com>
2025-11-24 22:39:33 +0530

    first commit

---
+++ 01-javascript-fundamentals/script.js
@@ -0,0 +1,9 @@
+let js = "Amazing";
+if (js === "Amazing") {
+  alert("Javascript is FUN!");
+}
+console.log("Hii");
+let firstName = "Sai";
+let _firstName = "Sai";
+let FisrtName = "Sai"; //not recommended
+// let 3_firstName = "Sai"; invalid
