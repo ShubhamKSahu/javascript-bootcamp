875bf617f0caf35b36a7a0ccf4ef4a3c526d513c
Shubham Kumar Sahu <shubham.official571@gmail.com>
2025-11-27 00:27:03 +0530

    basic dom manipulation using querySelector

---
+++ dom-manipulation/guess-my-number-project/script.js
@@ -0,0 +1,5 @@
+"use strict";
+document.querySelector(".number").textContent = 30;
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+document.querySelector(".message").textContent = "🎉 That's correct!";
