5909b680abf27ceff0b8d0c11edeb848e9eca74e
Shubham Kumar Sahu <shubham.official571@gmail.com>
2026-02-16 23:05:18 +0530

    keyboard events

--- event-listeners/03_basics/app.js
+++ event-listeners/03_basics/app.js
@@ -1,18 +1,34 @@
-let p = document.querySelector("p");
-let div = document.querySelector(".box");
+// let p = document.querySelector("p");
+// let div = document.querySelector(".box");
 let btn = document.querySelector("button");
-
-function addBackgroundColor(){
-    this.style.backgroundColor = "purple"
-}
-p.addEventListener("click", addBackgroundColor)
-
-div.addEventListener("click", function(){
-    console.log(this);
-    addBackgroundColor.call(this);
+let inp = document.querySelector("input");
+inp.addEventListener("keydown", (event) => {
+    console.log(event.code);
+    if(event.code == 'ArrowUp'){
+        console.log("move upward");
+    }
+    if(event.code == 'ArrowDown'){
+        console.log("move downward");
+    }
+    if(event.code == 'ArrowLeft'){
+        console.log("move left");
+    }
+    if(event.code == 'ArrowRight'){
+        console.log("move right");
+    }
 })
 
-btn.addEventListener("click", function(){
-    console.log(this);
-    addBackgroundColor.call(this);
-})
+// function addBackgroundColor(){
+//     this.style.backgroundColor = "purple"
+// }
+// p.addEventListener("click", addBackgroundColor)
+
+// div.addEventListener("click", function(){
+//     console.log(this);
+//     addBackgroundColor.call(this);
+// })
+
+// btn.addEventListener("click", function(){
+//     console.log(this);
+//     addBackgroundColor.call(this);
+// })
