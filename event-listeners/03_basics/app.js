3ffe6004ae45d7e49a9fde71356d49dac3b8cf6b
Shubham Kumar Sahu <shubham.official571@gmail.com>
2026-03-31 20:35:42 +0530

    form events

--- event-listeners/03_basics/app.js
+++ event-listeners/03_basics/app.js
@@ -1,22 +1,22 @@
 // let p = document.querySelector("p");
 // let div = document.querySelector(".box");
-let btn = document.querySelector("button");
-let inp = document.querySelector("input");
-inp.addEventListener("keydown", (event) => {
-    console.log(event.code);
-    if(event.code == 'ArrowUp'){
-        console.log("move upward");
-    }
-    if(event.code == 'ArrowDown'){
-        console.log("move downward");
-    }
-    if(event.code == 'ArrowLeft'){
-        console.log("move left");
-    }
-    if(event.code == 'ArrowRight'){
-        console.log("move right");
-    }
-})
+// let btn = document.querySelector("button");
+// let inp = document.querySelector("input");
+// inp.addEventListener("keydown", (event) => {
+//     console.log(event.code);
+//     if(event.code == 'ArrowUp'){
+//         console.log("move upward");
+//     }
+//     if(event.code == 'ArrowDown'){
+//         console.log("move downward");
+//     }
+//     if(event.code == 'ArrowLeft'){
+//         console.log("move left");
+//     }
+//     if(event.code == 'ArrowRight'){
+//         console.log("move right");
+//     }
+// })
 
 // function addBackgroundColor(){
 //     this.style.backgroundColor = "purple"
@@ -32,3 +32,15 @@ inp.addEventListener("keydown", (event) => {
 //     console.log(this);
 //     addBackgroundColor.call(this);
 // })
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let user = this.elements[0];
+
+     let password = this.elements[1];
+     console.log("username is: ", user.value);
+     console.log("password is: ", password.value);
+    alert(`Hi, ${user.value} your password is set to ${password.value}`);
+    console.dir(form);
+})
