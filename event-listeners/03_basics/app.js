7278d7d675e77c43c58200c26f3cbfeafc15959a
Shubham Kumar Sahu <shubham.official571@gmail.com>
2026-08-03 01:04:26 +0530

    event delegation

--- event-listeners/03_basics/app.js
+++ event-listeners/03_basics/app.js
@@ -33,20 +33,49 @@
 //     addBackgroundColor.call(this);
 // })
 
-let div = document.querySelector("div");
+// let div = document.querySelector("div");
+// let ul = document.querySelector("ul");
+// let lis = document.querySelectorAll("li");
+// div.addEventListener("click", (event) => {
+//     event.stopPropagation();
+//     console.log("div is clicked");
+// });
+// ul.addEventListener("click", (event) => {
+//     event.stopPropagation();
+//     console.log("ul is clicked");
+// }) ;  
+// for (li of lis){
+//     li.addEventListener("click", (event)=>{
+//         event.stopPropagation();
+//         console.log("li is clicked");
+//     })
+// } 
+let input = document.querySelector("input");
 let ul = document.querySelector("ul");
-let lis = document.querySelectorAll("li");
-div.addEventListener("click", (event) => {
-    event.stopPropagation();
-    console.log("div is clicked");
-});
-ul.addEventListener("click", (event) => {
-    event.stopPropagation();
-    console.log("ul is clicked");
-}) ;  
-for (li of lis){
-    li.addEventListener("click", (event)=>{
-        event.stopPropagation();
-        console.log("li is clicked");
-    })
-} 
+let button = document.querySelector("button");
+button.addEventListener("click", function(){
+    taskname = input.value;
+    let listitem  = document.createElement('li');
+    let dltbtn = document.createElement("button");
+    dltbtn.innerText = 'delete';
+    dltbtn.classList.add('delete');
+    listitem.textContent = taskname;
+    ul.append(listitem);
+    listitem.append(dltbtn);
+    input.value = "";
+})
+ul.addEventListener("click", function(event){
+    if( event.target.nodeName == 'BUTTON'){
+        dltbtn = document.querySelector('.delete');
+        dltbtn.parentElement.remove();
+        console.log('task deleted');
+    }
+})
+// let dltbtns = document.querySelectorAll(".delete");
+// for (dltbtn of dltbtns){
+//     dltbtn.addEventListener("click", function(){
+//     console.log("task is deleted");
+// })   
+// }
+
+
